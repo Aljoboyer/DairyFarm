8316ff653dce5d8ee4b1e0b766604d9b0f5a4185
Aljoboyer <tanveeazmain@gmail.com>
2022-04-04 22:59:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import HomeBanner from './Components/HomeBanner';
+import MilkTable from './Components/MilkTable';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <HomeBanner/>
+      <MilkTable/>
+    </main>
   );
 }
 
